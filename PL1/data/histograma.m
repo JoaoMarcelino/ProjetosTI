4d63267aa9cc6b09c0fcba37e3f4dfde21bbafe2
josphze <josphze@gmail.com>
2019-10-26 19:29:15 +0100

    Fuck you marc3elo

--- PL1/data/histograma.m
+++ PL1/data/histograma.m
@@ -1,11 +1,16 @@
 function [h,alpha]=histograma(f,alpha)
+
     f=f(:);
+    
     if (nargin==1)
         alpha=unique(f);
     end
+    
     h=zeros(size(alpha));
+    
     for k=1:length(alpha)
         i=find(f==alpha(k));
         h(k)=length(i);
     end
+    
 end
