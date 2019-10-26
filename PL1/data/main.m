bcf4d14941a91dea20e3db5b8a765b3d5f1a3a85
JoaoMarcelino <j.13.marcelino@gmail.com>
2019-10-26 15:45:09 +0100

    5 finished kinda, 6 started

--- PL1/data/main.m
+++ PL1/data/main.m
@@ -9,15 +9,17 @@ file=fopen("lyrics.txt","r");
 lyrics=fscanf(file,"%s");
 fclose(file);
 lyrics=regexprep(lyrics,"[^a-zA-Z0-9]","");
-%helper(lyrics,1);
-c=Conjuntos(lyrics);
-disp(c);
+%lyrics2=double(lyrics(:,1:2:end-1))*2^8+double(lyrics(:,2:2:end));
+%helper(lyrics2,1);
 
 % Landscape
-%helper(landscape);
+%landscape2=double(landscape(:,1:2:end-1))*2^8+double(landscape(:,2:2:end));
+%helper(landscape2);
 
 %MRI
 %helper(MRI);
+%MRI2=double(MRI(:,1:2:end-1)*2^8+double(MRI(:,2:2:end));
+%helper(MRI2);
 
 %MRIBW
 %helper(MRIBW);
@@ -26,6 +28,13 @@ disp(c);
 %helper(aud);
 
 
+%------------------%
+query=[2 6;4 9];
+target=[6 8 9 7;2 4 9 9; 4 9 1 4];
+alfabeto=0:9;
+step=1;
+a=rotina(query,target,alfabeto,step);
+disp(a);
 
 
 
---
+++ PL1/data/rotina.m
@@ -0,0 +1,13 @@
+function r=rotina(query,target,alfa,step)
+    rpos=1;
+    cpos=1;
+    for k=1:step:length(target)
+        r=target(rpos:size(query),cpos:size(query,2));
+        
+    %end
+    
+    X=entropia(query/sum(query);
+    y=entropia(t);
+    conjunta=double(X(:,1:2:end-1))*2^8+double(Y(:,2:2:end));
+
+end
