139e7acf2ce00a73991abfcf89e27b53f2ee33a3
josphze <josphze@gmail.com>
2019-10-28 13:33:32 +0000

    ex6c

--- PL1/data/rotina.m
+++ PL1/data/rotina.m
@@ -1,45 +1,37 @@
-function r = rotina(query,target,alfa,step)
+    function maxInfo = rotina(query,target,step,i)
 
-    row = 1;
     rowFinal = size(query,1);
-    column = 1;
     columnFinal = size(query,2);
+    entropiaQuery = helperRotina(query);
+    posInfoColumn = 1;
+    posInfoRow = 1;
+    matrizInformacoes = zeros(length(1 : step : size(target,1) - size(query,1) + 1),length(1 : step : size(target,2) - size(query,2) + 1));
+    maxInfo = 0;
     
-    % Numero de tentativas wait wtf
-    %n_repeticoes = (length(target) - length(query)+step) * (size(target) - size(query) + step); %lmao
-    
-    for k = 1 : n_repeticoes
-        r=target(row:rowFinal,column:columnFinal);
-        disp(r);
-        column= column + step;
-        columnFinal= columnFinal + step;
-        
-        % ok shiny o marcelo tem autismo e em vez de meter um for dentro de
-        % um for decidiu calcular o total de vezes e assim q chega ao fim
-        % puxa para baixo
-        % #lmao
-        % temos de encontrar uma maneira melhor de fazer isto :)
-        % but at least this works x)
-        if columnFinal>size(target,2)
-            column=1;
-            columnFinal=size(query,2);
-            row= row + step;
-            rowFinal= rowFinal + step;
+    for row = 1 : step : size(target,1) - size(query,1) + 1 
+        for column = 1 : step : size(target,2) - size(query,2) + 1 
+            
+            janela = target(row:row+rowFinal-1, column:column+columnFinal-1);
+            entropiaJanela = helperRotina(janela);
+            matrizConjunta = double(janela) * 2^8 + double(query);
+            entropiaConjunta = helperRotina(matrizConjunta);
+            
+            informacaoMutua = entropiaJanela + entropiaQuery - entropiaConjunta;
+            
+            if (informacaoMutua > maxInfo)
+                maxInfo = informacaoMutua;
+                x = column;
+                y = row;
+            end
+            matrizInformacoes(posInfoRow,posInfoColumn) = informacaoMutua;
+            
+            posInfoColumn = posInfoColumn + 1;
         end
-        
+        posInfoColumn = 1;
+        posInfoRow = posInfoRow + 1;
     end
+    figure(i);
+    imshow(target);
+    rectangle('Position', [x, y, columnFinal, rowFinal], 'LineWidth', 2, 'LineStyle', '--', 'EdgeColor', 'r');
     
-    %X=entropia(query/sum(query));  
-    %y=entropia(t);
-    %conjunta=double(X(:,1:2:end-1))*2^8+double(Y(:,2:2:end));
-    
-    
-    
-    
-    % Eu ate queria arranjar esta merda mas nunca vi uma fiesta de codigo
-    % tao grande ja sao 4:10 e o marcelino esta a falar de socos e de
-    % basket comigo no discord sobre os 76rs e eu a comentar esta merda
-    % toda, ainda n sei se tento ler isto ou nao, oh well, guess i'll try,
-    % ja que comentei as outras 200 linhas super detalhadamente nao posso
-    % parar assim #CleanAssCommentsDoJosphze
 end
